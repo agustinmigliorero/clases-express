c6d6967400eee7f73822c8e8e40dea46087b7129
Agustin Migliorero Tumini <56460586+agustinmigliorero@users.noreply.github.com>
2025-11-20 11:01:15 -0300

    Formulario conectado

--- nextjs-12-11/app/form/Form.jsx
+++ nextjs-12-11/app/form/Form.jsx
@@ -19,6 +19,27 @@ import {
 import { useState } from "react";
 
 function Form() {
+  const [titulo, setTitulo] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [contenido, setContenido] = useState("");
+  const [precio, setPrecio] = useState("");
+  const [imagen, setImagen] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    const respuesta = await fetch("http://localhost:3000/api/productos", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        titulo,
+        descripcion,
+        contenido,
+        precio,
+        imagen,
+      }),
+    });
+  }
+
   return (
     <FieldSet>
       <FieldLegend>Crear Producto</FieldLegend>
@@ -26,26 +47,55 @@ function Form() {
       <FieldGroup>
         <Field>
           <FieldLabel htmlFor="titulo">Titulo</FieldLabel>
-          <Input id="titulo" autoComplete="off" placeholder="Producto 1" />
+          <Input
+            value={titulo}
+            id="titulo"
+            onChange={(e) => setTitulo(e.target.value)}
+            autoComplete="off"
+            placeholder="Producto 1"
+          />
         </Field>
         <Field>
           <FieldLabel htmlFor="descripcion">Descripcion</FieldLabel>
-          <Input id="descripcion" autoComplete="off" />
+          <Input
+            onChange={(e) => setDescripcion(e.target.value)}
+            value={descripcion}
+            id="descripcion"
+            autoComplete="off"
+          />
         </Field>
         <Field>
           <FieldLabel htmlFor="contenido">Contenido</FieldLabel>
-          <Input id="contenido" autoComplete="off" />
+          <Input
+            onChange={(e) => setContenido(e.target.value)}
+            value={contenido}
+            id="contenido"
+            autoComplete="off"
+          />
         </Field>
         <Field>
           <FieldLabel htmlFor="precio">Precio</FieldLabel>
-          <Input id="precio" type="number" autoComplete="off" />
+          <Input
+            onChange={(e) => setPrecio(e.target.value)}
+            value={precio}
+            id="precio"
+            type="number"
+            autoComplete="off"
+          />
         </Field>
         <Field>
           <FieldLabel htmlFor="imagen">Imagen</FieldLabel>
-          <Input id="imagen" autoComplete="off" />
+          <Input
+            onChange={(e) => setImagen(e.target.value)}
+            value={imagen}
+            id="imagen"
+            autoComplete="off"
+          />
         </Field>
       </FieldGroup>
-      <Button variant="">Enviar</Button>
+      <Button onClick={handleSubmit} variant="">
+        Enviar
+      </Button>
     </FieldSet>
   );
 }
